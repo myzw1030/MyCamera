a17c8f4f32caa9fecf370392f0f21dc9c85bf98a
tommy <tmtmtoa@gmail.com>
2023-07-05 23:01:36 +0900

    カメラ起動

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -3,14 +3,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 撮影した写真を保持する状態変数
+    @State var captureImage: UIImage? = nil
+    // 撮影画面(sheet)の開閉状態を管理
+    @State var isShowSheet = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            // 撮影した写真がある時
+            if let captureImage {
+                Image(uiImage: captureImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Spacer()
+            Button {
+                // カメラが利用可能かチェック
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("カメラは利用できます")
+                    isShowSheet.toggle()
+                } else {
+                    print("カメラは利用できません")
+                }
+            } label: {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
         }
-        .padding()
     }
 }
 
