fa0deb78571e1b881596d8156fe0f81dd9963e40
tommy <tmtmtoa@gmail.com>
2023-07-06 23:04:01 +0900

    シェア機能

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -39,6 +39,28 @@ struct ContentView: View {
             .sheet(isPresented: $isShowSheet) {
                 ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
             }
+            
+            // captureImageをアンラップする
+            if let captureImage {
+                // captureImageから共有する画像を生成する
+                let shareImage = Image(uiImage: captureImage)
+                // 共有シート
+                ShareLink(item: shareImage, subject: nil, message: nil,
+                          preview: SharePreview("Photo", image: shareImage)) {
+                    // テキスト表示
+                    Text("SNSに投稿する")
+                    // 横幅いっぱい
+                        .frame(maxWidth: .infinity)
+                    // 高さ50ポイント指定
+                        .frame(height: 50)
+                    // 背景を青色に指定
+                        .background(Color.blue)
+                    // 文字色を白色に指定
+                        .foregroundColor(Color.white)
+                    // 上下左右に余白を追加
+                        .padding()
+                } // ShareLinkここまで
+            }
         }
     }
 }
